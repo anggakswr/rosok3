d23bc54ec5bebc41de9010c1e613618efd183f7a
anggakswr <222180023@student.unaki.ac.id>
2020-06-16 21:29:02 +0700

    memperbaiki ukuran nav & header saat responsive, menambah button & label utk navigasi slider (tp blm jalan), menambah slider product tp blm jalan

---
+++ script.js
@@ -0,0 +1,19 @@
+// // jika slidenav diklik
+// let i = 0;
+// const slidenav = document.getElementsByTagName('input');
+// // // slidenav.forEach(radio => {
+// // //     radio.addEventListener("click", function () {
+// // //         console.log('boom');
+// // //     });
+// // // });
+
+// // for (let i = 0; i < slidenav.length; i++) {
+// //     slidenav[i].addEventListener("click", function () {
+// //         slidenav[i].classList.remove('active');
+// //         this.classList.add('active');
+// //     });
+// // }
+
+// if (slidenav[i].checked) {
+//     console.log(slidenav[i]);
+// }
