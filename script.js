c0900298a65e28edd1031a4d02ce041ffee2a77d
anggakswr <222180023@student.unaki.ac.id>
2020-06-20 21:02:47 +0700

    memperkecil gambar product-slider saat mobile, memperbaiki fitur slider, mengembalikan ukuran p saat mobile

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
-// // jika slidenav diklik
-// let i = 0;
-// const slidenav = document.getElementsByTagName('input');
-// // // slidenav.forEach(radio => {
-// // //     radio.addEventListener("click", function () {
-// // //         console.log('boom');
-// // //     });
-// // // });
+// menyeleksi container dr gambar yg akan di slide
+const slide = document.getElementsByClassName('slide')[0];
 
-// // for (let i = 0; i < slidenav.length; i++) {
-// //     slidenav[i].addEventListener("click", function () {
-// //         slidenav[i].classList.remove('active');
-// //         this.classList.add('active');
-// //     });
-// // }
+// ambil elemen input dg type radio
+const radios = document.querySelectorAll('input[type=radio]');
 
-// if (slidenav[i].checked) {
-//     console.log(slidenav[i]);
-// }
+const labels = Array.from(document.getElementsByTagName('label'));
+
+// deklarasi variabel utk looping
+let i = 0;
+
+// function utk slide gambar
+function slideOtomatis() {
+    // remove class active di smua label
+    labels.forEach(label => {
+        label.classList.remove('active');
+    });
+
+    radios[i].click();
+
+    // jika i = 5 - 1
+    if (i < radios.length - 1) {
+        // 1 + 1
+        i++;
+    } else {
+        // i direset jd 0
+        i = 0;
+    }
+
+    // berjalan setiap 3 detik
+    setTimeout("slideOtomatis()", 3000);
+
+}
+
+// jalankan function
+window.onload = slideOtomatis();
+
+// looping radios
+for (let i = 0; i < radios.length; i++) {
+    // jika radio button diklik
+    radios[i].addEventListener('click', function () {
+        slide.style.transform = "translateX(-" + i * 20 + "%)";
+        labels[i].classList.add('active');
+    });
+}
